2ae7c70f6d7985ce5587ff72052189de0065bff4
motoki higa <e225715@ie.u-ryukyu.ac.jp>
2024-01-26 23:48:29 +0900

    lecture finished

--- boardapp/models.py
+++ boardapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class BoardModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.CharField(max_length=50)
+    snsimage = models.ImageField(upload_to='')
+    good = models.IntegerField(blank=True, null=True, default=0)
+    read = models.IntegerField(blank=True, null=True, default=0)
+    readtext = models.TextField(blank=True, null=True, default='')
+
+    def __str__(self):
+        return self.title
--- boardproject/settings.py
+++ boardproject/settings.py
@@ -122,3 +122,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [str(BASE_DIR / 'static')]
+
+STATIC_URL = '/sta/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = 'medi/'
+
+LOGIN_URL = 'login'
